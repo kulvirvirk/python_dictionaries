ff73cba82396199ec446cba3fff98b7919a9a06d
kulvirvirk <kvirkdev@gmail.com>
2020-02-16 04:21:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# 1. create and print dictionary that contain fruits
+#    keys are nubmer and value are fruit names  
+# 2. 
+
+my_fruit_dictionary = {
+ 1:'mango',
+ 2:'apple',
+ 3:'kiwi',
+ 4:'orange',
+ 5:'banana'
+}
+ 
+print(my_fruit_dictionary)
