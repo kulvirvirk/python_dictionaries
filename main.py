a8399cf5ddbd401a5309fc84cdd3b3b3845356de
kulvirvirk <kvirkdev@gmail.com>
2020-02-16 04:47:36 +0000

    added an example of copying a dictionary to new dictionary

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # 1. create and print dictionary that contain fruits
 #    keys are nubmer and value are fruit names  
 # 2. add values() method to fina a 'mango' in dictionary
+# 3. copy the dictionary to new dictionary by using copy() method 
 
 my_fruit_dictionary = {
  1:'mango',
@@ -14,5 +15,14 @@ my_fruit_dictionary = {
 print(my_fruit_dictionary)
 
 print(my_fruit_dictionary.values())
+print('--------------******--------------\n')
 
+# 2. add values() method to fina a 'mango' in dictionary
 print('mango' in my_fruit_dictionary.values())
+print('--------------******--------------\n')
+
+# 3. copy the dictionary to new dictionary by using copy() method 
+my_fruit_dictionary_copy = my_fruit_dictionary.copy
+print(my_fruit_dictionary)
+print(my_fruit_dictionary_copy)
+print('--------------******--------------\n')
