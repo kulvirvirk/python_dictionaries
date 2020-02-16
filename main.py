9c458d852905e042bc41c2a783e5be637a7fcc15
kulvirvirk <kvirkdev@gmail.com>
2020-02-16 04:43:43 +0000

    added an example of values() method

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 # 1. create and print dictionary that contain fruits
 #    keys are nubmer and value are fruit names  
-# 2. 
+# 2. add values() method to fina a 'mango' in dictionary
 
 my_fruit_dictionary = {
  1:'mango',
@@ -12,3 +12,7 @@ my_fruit_dictionary = {
 }
  
 print(my_fruit_dictionary)
+
+print(my_fruit_dictionary.values())
+
+print('mango' in my_fruit_dictionary.values())
