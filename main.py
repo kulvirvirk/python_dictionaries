0f0d294158496355782304301cb1172314909b67
kulvirvirk <kvirkdev@gmail.com>
2020-04-16 05:21:59 +0000

    fixed the .copy()

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #    keys are nubmer and value are fruit names  
 # 2. add values() method to fina a 'mango' in dictionary
 # 3. copy the dictionary to new dictionary by using copy() method 
-# 4. remove the element '4' from the dictionary 
+# 4. remove the element '2' and '4' from the dictionary 
 # 5. use update() method to update the dictionary with another fruit e.g. 6:'watermelon'
 
 my_fruit_dictionary = {
@@ -24,13 +24,14 @@ print('mango' in my_fruit_dictionary.values())
 print('--------------******--------------\n')
 
 # 3. copy the dictionary to new dictionary by using copy() method 
-my_fruit_dictionary_copy = my_fruit_dictionary.copy
+my_fruit_dictionary_copy = my_fruit_dictionary.copy()
 print(my_fruit_dictionary)
 print(my_fruit_dictionary_copy)
 print('--------------******--------------\n')
 
 # 4. remove the element '4' from the dictionary 
 print(my_fruit_dictionary)
+print(my_fruit_dictionary.pop(2))
 print(my_fruit_dictionary.pop(4))
 print(my_fruit_dictionary)
 print('--------------******--------------\n')
